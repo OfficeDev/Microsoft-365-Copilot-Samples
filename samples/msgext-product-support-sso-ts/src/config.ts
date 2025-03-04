0aef740d6e1e79c68e021e7fe613b3dbe2439c4f
Garry Trinder <garry@trinder365.co.uk>
2025-03-04 14:04:40 +0000

    Remove message extension plugins (#201)

--- samples/msgext-product-support-sso-ts/src/config.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  botId: process.env.BOT_ID,
-  botPassword: process.env.BOT_PASSWORD,
-  connectionName: process.env.CONNECTION_NAME,
-  sharepointHost: process.env.SPO_HOSTNAME,
-  sharepointSite: process.env.SPO_SITE_URL,
-};
-
-export default config;
